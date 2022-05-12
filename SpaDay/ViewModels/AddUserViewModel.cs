ee64c0d3db735fba8dabbe4bb1228b42fa857b5e
Priyanka <priyanka.msharma@gmail.com>
2022-05-12 18:30:30 -0400

    Commit trial

---
+++ SpaDay/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,32 @@
+﻿using SpaDay.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpaDay.ViewModels
+{
+    public class AddUserViewModel
+    {
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Username must be between 5 and 15 characters long")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Sorry, but the given password is too short. Passwords must be at least 6 characters long.")]
+        public string Password { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Verify Password is required")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Sorry, passwords does not matches.")]
+        public string VerifyPassword { get; set; }  
+
+        public AddUserViewModel()
+        {
+            Username = Username;
+            Password = Password;
+            Email = Email;
+            VerifyPassword = VerifyPassword;
+        }
+    }
+}
